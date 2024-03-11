e0e015ceb23a08c376d890ecd4d79a14761a2339
Bloddy20Moon <118792974+Bloddy20Moon@users.noreply.github.com>
2024-03-10 19:23:26 -0500

    Parte_1

---
+++ vistas/modulos/main.php
@@ -0,0 +1,71 @@
+<!-- Left Panel -->
+<aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="index.php"><i class="menu-icon fa fa-laptop"></i>INICIO</a>
+                    </li>
+                    <li class="menu-title">MENU</li>
+                    <!-- /.menu-title -->
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="menu-icon fa fa-cogs"></i>Menu
+                        </a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-address-book"></i><a href="estudiante">Estudiante</a></li>
+                            <li><i class="fa fa-address-card-o" aria-hidden="true"></i><a href="docente">Docente</a></li>
+                            <li><i class="fa fa-bars"></i><a href="aulas">Aulas</a></li>
+                            <li><i class="fa fa-address-book"></i><a href="calificaciones">Calificaciones</a></li>
+                            <li><i ></i><a href="materias">Materias</a></li>
+                            <li><i class="fa fa-calendar-o" aria-hidden="true"></i><a href="horarios">Horarios</a></li>
+                        </ul>
+                    </li>
+                    <!-- /.menu-title -->
+
+
+                    <!-- .herramienta-title -->
+
+                    <li class="menu-title">Herramientas</li>
+
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                             <i class="menu-icon fa fa-tasks"></i>Herramientas</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-fort-awesome"></i><a href="font-fontawesome.html">Font Awesome</a></li>
+                            <li><i class="menu-icon ti-themify-logo"></i><a href="font-themify.html">Themefy Icons</a></li>
+                        </ul>
+                    </li>
+                    
+                       <!-- ./herramienta-title -->
+
+                          <!-- .Supervisar del-title -->
+
+                    <li class="menu-title">Supervisar</li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Ver</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-sign-in"></i><a href="estudiantes">Estudiantes</a></li>
+                            <li><i class="menu-icon fa fa-sign-in"></i><a href="profesores">Profesores</a></li>
+                            <li><i class="menu-icon fa fa-paper-plane"></i><a href="notas">Calificaciones</a></li>
+                        </ul>
+                    </li>   
+                        <!-- /.Supervisar del-title -->
+
+
+                        <!-- .Extras del-title -->
+
+                    <li class="menu-title">Extras</li><!-- /.menu-title -->
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Extras</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-sign-in"></i><a href="cerrarsesi">Cerrar Sesion</a></li>
+                            <li><i class="fa fa-user-o" aria-hidden="true"></i><a href="registrar">Registrar Usuario</a></li>
+                         
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.Extras-collapse -->
+        </nav>
+    </aside>
+    <!-- /#left-panel -->
---
+++ vistas/modulos/menu/estudiante.php
@@ -0,0 +1,1 @@
+<h1>ESTUDIOS</h1>
